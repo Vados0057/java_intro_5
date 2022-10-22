fae83c4dabda6d8c87e9323f0deb7052dfcb176b
Vladyslav Ksendziuk <vlad.borland13@gmail.com>
2022-10-22 13:57:05 -0500

    added

--- src/arrays/_01_StringArray.java
+++ src/arrays/_01_StringArray.java
@@ -20,10 +20,33 @@ public class _01_StringArray {
         System.out.println(Arrays.toString(countries)); //[null, Spain, null]
 
         /*
-        Assign Belgium to the index of 2 and print the array agian
+        Assign Belgium to the index of 2 and print the array again
         */
 
         countries[2] = "Belgium";
         System.out.println(Arrays.toString(countries)); //[null, Spain, Belgium]
-    }
+
+        // 5. Update existed element
+        countries[1] = "France"; // we update "Spain" to "France".
+        System.out.println(Arrays.toString(countries)); // [null, France, Belgium]
+
+        // 6. Getting the length of an array - how many elements -3
+        int i = countries.length; // Same with next one
+        System.out.println(i);
+        System.out.println(countries.length); //3
+
+        // ARRAYS CAN'T BE MODIFIED
+
+        // countries[3] = "German"; // ArrayIndexOutOfBoundsException
+        // System.out.println(Arrays.toString(countries));
+
+        // 7. Printing each element's separately
+
+        for (int j = 0; j < countries.length; j++) {
+            System.out.println("Country at index of " + i + " is = " + countries[j]);
+        }
+//        System.out.println(countries[0]);
+//        System.out.println(countries[1]);
+//        System.out.println(countries[2]);
+
 }
